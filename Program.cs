07e66b85ccf663157b3cabc50243a13d297c9b02
Igor Golovin <ingvarr_rus@list.ru>
2024-01-25 14:51:46 +0300

    Task 3 lesson 4

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+//  Задача №3: Напишите программу, которая перевернёт одномерный массив (первый элемент станет последним
+//  второй – предпоследним и т.д.)
+
+int[] CreateArray(int size, int minRange, int maxRange) // создаём метод создания массива из случайных чисел
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(minRange, maxRange + 1);
+    }
+    return array; // конец метода CreateArray
+}
+int minRange = 10;
+int maxRange = 99;
+Console.Write("Введём размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] arrNew = CreateArray(size, minRange, maxRange);
+Console.WriteLine($"Массив : [{string.Join("; ", arrNew)}]");
+
+for (int i = arrNew.Length - 1; i>= 0; i--)
+{
+       Console.Write(arrNew[i] + "; ");
+}
+
